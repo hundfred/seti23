48592b1b06736c737b49dd1642d6198663bb5ff4
Manfred Hund <hundfred@s23.org>
2011-12-28 16:22:04 +0100

    seti23 apk for android

--- gen/org/seti23/R.java
+++ gen/org/seti23/R.java
@@ -15,14 +15,24 @@ public final class R {
         public static final int s23=0x7f020001;
     }
     public static final class id {
-        public static final int editText1=0x7f050001;
-        public static final int test_image=0x7f050000;
+        public static final int button_irc=0x7f050002;
+        public static final int button_ticket=0x7f050003;
+        public static final int greeting_field=0x7f050000;
+        public static final int name_field=0x7f050001;
+        public static final int test_image=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int button_irc=0x7f040003;
+        public static final int button_ticket=0x7f040004;
+        public static final int eingabefeld=0x7f040002;
+        public static final int error_name_missing=0x7f040005;
+        public static final int error_ok=0x7f040006;
         public static final int hello=0x7f040000;
+        public static final int hello_greeting=0x7f040008;
+        public static final int hi_greeting=0x7f040007;
     }
 }
--- src/org/seti23/Seti23.java
+++ src/org/seti23/Seti23.java
@@ -1,11 +1,18 @@
 package org.seti23;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 //import android.widget.AnalogClock;
 
-public class Seti23 extends Activity {
+public class Seti23 extends Activity implements OnClickListener {
+	private Button button_irc;
+	private Button button_ticket;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,45 @@ public class Seti23 extends Activity {
         ImageView image = (ImageView) findViewById(R.id.test_image);
         image.setImageResource(R.drawable.s23);        
     
-        
+        button_irc = (Button)findViewById(R.id.button_irc);
+        button_irc.setOnClickListener(this);
+        button_ticket = (Button)findViewById(R.id.button_ticket);
+        button_ticket.setOnClickListener(this);    
     }
+	public void onClick(View v) {
+		EditText nameField = (EditText) findViewById(R.id.name_field);
+		String name = nameField.getText().toString();
+		if (name.length() == 0) {
+		    new AlertDialog.Builder(this) 
+		            .setMessage(R.string.error_name_missing)
+		            .setNeutralButton(R.string.error_ok, null)
+		            .show();
+		    return;
+		}
+		
+		else {
+			if ( v==button_irc ) {
+				name=name+"_irc_button";
+			}
+			
+			if ( v==button_ticket ) {
+				name=name+"_ticket_button";
+			}
+			new AlertDialog.Builder(this) 			
+			.setMessage(name)
+			.setNeutralButton(R.string.error_ok, null)
+			.show();
+			return;
+		}
+			
+			
+			
+			
+			
+		
+		
+		// TODO Auto-generated method stub
+		
+	}
+    
 }
